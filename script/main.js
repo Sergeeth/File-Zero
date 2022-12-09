ea1e0660931d3ab0f159a795712b56ba9078504d
Sergeeth <62308595+Sergeeth@users.noreply.github.com>
2022-12-09 13:51:07 +0200

    Update

--- script/main.js
+++ script/main.js
@@ -112,3 +112,52 @@ $('.play-circle.b').mouseleave(function (e) {
     })
 
 });
+
+// slider
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+
+$('.slider-nav').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    focusOnSelect: true,
+    responsive: [
+        {
+          breakpoint: 1247,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+    ]
+});
+
+$('a[data-slide]').click(function(e) {
+    e.preventDefault();
+    var slideno = $(this).data('slide');
+    $('.slider-nav').slick('slickGoTo', slideno - 1);
+});
