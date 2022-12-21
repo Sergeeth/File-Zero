8c71b8c7d27ef806068b17010498d9d20d6649fb
Sergeeth <62308595+Sergeeth@users.noreply.github.com>
2022-12-21 18:18:02 +0200

    Update

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
         disableOn: 700,
         type: 'iframe',
@@ -142,34 +142,78 @@ $('.slider-nav').slick({
     autoplay: true,
     autoplaySpeed: 4000,
     focusOnSelect: true,
-    responsive: [
-        {
-          breakpoint: 1247,
-          settings: {
-            slidesToShow: 5,
-            slidesToScroll: 1,
-            infinite: true,
-          }
+    responsive: [{
+            breakpoint: 1247,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                infinite: true,
+            }
         },
         {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1
-          }
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
         },
         {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
         }
     ]
 });
 
-$('a[data-slide]').click(function(e) {
+$('a[data-slide]').click(function (e) {
     e.preventDefault();
     var slideno = $(this).data('slide');
     $('.slider-nav').slick('slickGoTo', slideno - 1);
 });
+
+// modal
+const modalShow = (modal) => {
+    $(`.modal[modal=${modal}]`).fadeIn().css("display", "flex");
+
+    function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING) {
+            alert('играет');
+        }
+    }
+};
+
+const modalHide = (modal) => {
+    $(`.modal[modal=${modal}]`).fadeOut();
+};
+
+$(".modal").each(function () {
+    const modal = $(this).attr("modal");
+    const opentime = $(this).attr("opentime");
+    const hidetime = $(this).attr("hidetime");
+
+    if (opentime) {
+        setTimeout(() => {
+            modalShow(modal);
+            if (hidetime) {
+                setTimeout(() => {
+                    modalHide(modal);
+                }, hidetime);
+            }
+        }, opentime);
+    };
+
+    $(".open-modal").on("click", function (e) {
+        const target = $(this).attr("modal");
+        e.preventDefault();
+        modalShow(target);
+    });
+
+    $(".modal-hide").on("click", function (e) {
+        const target = $(this).attr("modal");
+        e.preventDefault();
+        modalHide(target);
+    });
+});
+
